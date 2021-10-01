9345b1f21f8e732d3c88114c401f0bff27f74883
jtcarter2314 <carterj2314@gmail.com>
2021-10-01 11:56:35 -0500

    .

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -18,7 +18,7 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tile
 streets.addTo(map);
 
 // Accessing the airport GeoJSON URL
-let airportData = "https://raw.githubusercontent.com/<GitHub_name>/Mapping_Earthquakes/main/majorAirports.json";
+let airportData = "https://raw.githubusercontent.com/jtcarter2314/Mapping_Earthquakes/main/majorAirports.json";
 
 
 // Grabbing our GeoJSON data.
